778d94bbca0a8427f3880dda396eab3d8e84010a
Brian Salemi <briandsalemi@gmail.com>
2019-05-17 15:39:41 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,4 +1,3 @@
-require 'pry'
 
 def sort_array_asc(array)
   array.sort
@@ -25,17 +24,21 @@ def kesha_maker(array)
   array.each do |word|
     word[2] = "$"
   end
+  dollar_sign = "$"
+  kesha = []
+  array.each do |word|
+    word[2] = dollar_sign
+    kesha << word
+  end
+kesha
 end
 
 def find_a(array)
-  a_words = []
-  array.collect do |word|
-    if word.start_with?("a")
-      a_words << word
+  array.find_all do |word|
+    word[0] == "a"
     end
   end
-  a_words
-end
+
 
 def sum_array(array)
   sum = 0
@@ -45,12 +48,13 @@ def sum_array(array)
   sum
 end
 
+
 def add_s(array)
   array.collect do |word|
     if array[1] == word
       word
     else
-       word + "s"
+      word + "s"
     end
   end
 end
